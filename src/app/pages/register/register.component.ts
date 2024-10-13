8e5451b1e3e57708be06c66a6e77b2824cd6e259
angelacardenas <angelacardenasl07@gmail.com>
2024-10-13 12:53:34 -0500

    Se tipo servicio de autenticacion para el registro, se envian mensajes de error al componente de registro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -7,28 +12,44 @@ import { AuthService } from '../../services/auth.service';
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-
   formData!: FormGroup;
+  message!: string;
 
-  constructor( private authService: AuthService ) {
+  constructor(private authService: AuthService) {
     this.formData = new FormGroup({
-      name: new FormControl( '', [ Validators.required ] ),
-      username: new FormControl( '', [ Validators.required, Validators.email ] ),
-      password: new FormControl( '', [ Validators.required, Validators.minLength( 8 ), Validators.maxLength( 20 ) ] )
+      name: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20),
+      ]),
     });
   }
 
   handleSubmit() {
-    if(this.formData.valid) {
+    if (this.formData.valid) {
       console.log(this.formData.value);
-      this.authService.registerUser( this.formData.value ).subscribe( function( data ) {
-        console.log(data);
-      });
+      this.authService
+        .registerUser(this.formData.value)
+        .subscribe((data: boolean | string) => {
+          console.log(data);
+          if (data == 'error') {
+            this.message = 'error en el servidor';
+          } else if (!data) {
+            this.message = 'ya existe el usuario';
+          } else {
+            this.message = 'Ha sido registrado exitosamente';
+          }
+
+          setTimeout(() => {
+            this.message = '';
+          }, 2000);
+        });
       this.formData.reset();
     }
   }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+import { Response } from '../interfaces/response';
 
 interface User { name: string, username: string, password: string, role?:string }
 
@@ -12,8 +14,21 @@ export class AuthService {
   /** Forma anterior a la version Angular 16 */
   constructor( private http: HttpClient ) { }
 
-  registerUser ( newUser: User ) {
-    return this.http.post( 'http://localhost:3000/api/auth/register', newUser );
+  registerUser ( newUser: User ) :Observable <boolean|string>{
+    return this.http.post <Response>( 'http://localhost:3000/api/auth/register', newUser ) .pipe( 
+      
+      map ((data)=>{
+        return data.ok
+      }),
+      catchError ((error)=>{
+        // console.log(error);
+        return of ("error")
+      })
+
+
+    );
+
+
   }
 }
   

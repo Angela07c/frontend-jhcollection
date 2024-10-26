97fb0235a43c2941a13be8314d05d67538a6f602
Duban Lopez <nlopezr6@ucentral.edu.co>
2024-10-25 19:16:04 -0500

    Se agrego el crud de product-list y muestra las tablas de producto

--- src/app/pages/products/product-list/product-list.component.ts
+++ src/app/pages/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
+  products: any [] = [];
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(){
+    this.productService.getAllProducts().subscribe((data) => {
+      console.log(data);
+      this.products = data.data
+    })
+    
+  }
 
 }
